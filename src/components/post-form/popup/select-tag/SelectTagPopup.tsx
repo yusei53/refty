d6adbdbc1b57ea522c561f61a77ec59de96661e6
yusei53 <yusei530922@gmail.com>
2024-12-22 16:26:10 +0900

    fix: popupをいい感じに閉じたりする

--- src/components/post-form/popup/select-tag/SelectTagPopup.tsx
+++ src/components/post-form/popup/select-tag/SelectTagPopup.tsx
@@ -1,5 +1,7 @@
-import { Box, Fade, Popper } from "@mui/material";
-import { label, TagButton } from "./button/TagButton";
+import CloseIcon from "@mui/icons-material/Close";
+import TagIcon from "@mui/icons-material/Tag";
+import { Box, Fade, Popper, Typography } from "@mui/material";
+import { TagButton } from "./button/TagButton";
 import { Button } from "@/src/components/ui/shared/button";
 import { theme } from "@/src/utils/theme";
 
@@ -11,13 +13,18 @@ type TagSelectionPopupProps = {
   onToggle: (event: React.MouseEvent<HTMLElement>) => void;
 };
 
-const tags = [
-  "学び",
-  "気づき",
-  "ひとりごと",
-  "1日の振り返り",
-  "インプットの記録"
-];
+const tags = ["学び", "気づき", "ひとりごと", "振り返り", "インプットの記録"];
+
+const label = {
+  fontSize: 13.5,
+  letterSpacing: 0.8,
+  height: "30px",
+  p: "4px 7px",
+  borderRadius: 2,
+  border: "1px solid #DCDFE3",
+  backgroundColor: "white"
+};
+
 export const SelectTagPopup: React.FC<TagSelectionPopupProps> = ({
   selectedTags,
   onChange,
@@ -26,24 +33,58 @@ export const SelectTagPopup: React.FC<TagSelectionPopupProps> = ({
   onToggle
 }) => {
   const toggleTag = (tag: string) => {
-    onChange((prevTags) =>
-      prevTags.includes(tag)
-        ? prevTags.filter((t) => t !== tag)
-        : [...prevTags, tag]
-    );
+    onChange((prevTags) => {
+      if (prevTags.includes(tag)) {
+        return prevTags.filter((t) => t !== tag);
+      }
+      if (prevTags.length >= 2) {
+        return prevTags;
+      }
+      return [...prevTags, tag];
+    });
   };
 
   return (
     <>
       <Box display={"flex"} alignItems={"center"}>
-        <Button onClick={onToggle}>タグ</Button>
-        <Box display={"flex"} gap={1}>
-          {selectedTags.map((tag) => (
-            <Box key={tag} sx={label}>
+        <Button
+          onClick={onToggle}
+          sx={{
+            border: "none",
+            bgcolor: `${theme.palette.primary.main}`,
+            borderRadius: 2,
+            height: "30px"
+          }}
+        >
+          <TagIcon sx={{ color: `${theme.palette.grey[500]}`, fontSize: 18 }} />
+          タグ
+        </Button>
+        {selectedTags.map((tag) => (
+          <Box key={tag} display={"flex"} gap={0.5} mx={0.5} zIndex={3}>
+            <Box display={"flex"} alignItems={"center"} sx={label}>
               {tag}
+              <Box
+                component={"button"}
+                bgcolor={"transparent"}
+                border={"none"}
+                p={0}
+                display={"flex"}
+                alignItems={"center"}
+                sx={{
+                  cursor: "pointer"
+                }}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onChange((prevTags) => prevTags.filter((t) => t !== tag));
+                }}
+              >
+                <CloseIcon
+                  sx={{ color: `${theme.palette.grey[500]}`, fontSize: 15 }}
+                />
+              </Box>
             </Box>
-          ))}
-        </Box>
+          </Box>
+        ))}
       </Box>
       <Popper
         open={open}
@@ -55,18 +96,29 @@ export const SelectTagPopup: React.FC<TagSelectionPopupProps> = ({
         {({ TransitionProps }) => (
           <Fade {...TransitionProps} timeout={250}>
             <Box
-              padding={1.2}
-              gap={1}
+              p={1}
               maxWidth={"280px"}
-              display={"flex"}
-              flexWrap={"wrap"}
-              bgcolor={`${theme.palette.primary.main}`}
+              bgcolor={`#f8fbff`}
               border={`1px solid ${theme.palette.grey[400]}`}
               borderRadius={4}
             >
-              {tags.map((tag) => (
-                <TagButton key={tag} tag={tag} onClick={() => toggleTag(tag)} />
-              ))}
+              <Typography
+                component={"span"}
+                sx={{ color: `${theme.palette.grey[500]}` }}
+                fontSize={12}
+                ml={1}
+              >
+                2つまで選択できます
+              </Typography>
+              <Box display={"flex"} flexWrap={"wrap"} gap={1} mt={0.5}>
+                {tags.map((tag) => (
+                  <TagButton
+                    key={tag}
+                    tag={tag}
+                    onClick={() => toggleTag(tag)}
+                  />
+                ))}
+              </Box>
             </Box>
           </Fade>
         )}
