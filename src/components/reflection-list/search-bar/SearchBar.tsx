c8bd7681d081624b45e7a74b26bb0c1b31fbcf3d
yusei53 <yusei530922@gmail.com>
2024-12-23 23:42:59 +0900

    feat: SearchBar

---
+++ src/components/reflection-list/search-bar/SearchBar.tsx
@@ -0,0 +1,56 @@
+import SearchIcon from "@mui/icons-material/Search";
+import { Box, IconButton, Stack, Slide } from "@mui/material";
+import { TagButton } from "../../post-form/popup/select-tag/button/TagButton";
+import { theme } from "@/src/utils/theme";
+
+type SearchBarProps = {
+  tags: string[];
+  selectedTag: string | null;
+  showTags: boolean;
+  onToggleTags: () => void;
+  onTagClick: (tag: string) => void;
+};
+
+export const SearchBar: React.FC<SearchBarProps> = ({
+  tags,
+  selectedTag,
+  showTags,
+  onToggleTags,
+  onTagClick
+}) => {
+  return (
+    <Box display={"flex"} alignItems={"center"} mx={3}>
+      <IconButton
+        aria-label={"タグで検索"}
+        onClick={onToggleTags}
+        sx={{
+          bgcolor: `${theme.palette.grey[100]}`,
+          borderRadius: 10,
+          fontSize: 14
+        }}
+      >
+        <SearchIcon sx={{ fontSize: 23 }} />
+      </IconButton>
+      <Box ml={1} overflow="hidden">
+        <Stack direction="row" gap={1.5}>
+          {tags.map((tag, index) => (
+            <Slide
+              key={tag}
+              direction="right"
+              in={showTags}
+              timeout={300 + index * 100}
+            >
+              <Box>
+                <TagButton
+                  tag={tag}
+                  onClick={() => onTagClick(tag)}
+                  selected={selectedTag === tag}
+                />
+              </Box>
+            </Slide>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
+  );
+};
