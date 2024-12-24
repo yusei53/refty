88e493ca4e18b8e87fd949e387121687551fadc7
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-12-24 21:17:30 +0900

    fix:option.ts

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import DiscordProvider from "next-auth/providers/discord";
 import GoogleProvider from "next-auth/providers/google";
 import LineProvider from "next-auth/providers/line";
+import TwitterProvider from "next-auth/providers/twitter";
 import type { NextAuthOptions } from "next-auth";
 import prisma from "@/src/lib/prisma";
 
