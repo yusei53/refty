7de7cbe9e7f08d32c9e1141809cbc90cb9f07a9d
yusei53 <yusei530922@gmail.com>
2025-01-02 13:48:52 +0900

    feat: Profile GET API

---
+++ src/app/api/[username]/profile-setting/route.ts
@@ -0,0 +1,47 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import prisma from "@/src/lib/prisma";
+import { getUserIdByUsername } from "@/src/utils/actions/get-userId-by-username";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { username: string } }
+) {
+  try {
+    const { username } = params;
+
+    const userId = await getUserIdByUsername(username);
+
+    if (!userId) {
+      return NextResponse.json(
+        { message: "ユーザーが見つかりません" },
+        { status: 404 }
+      );
+    }
+
+    const profile = await prisma.user.findUnique({
+      where: {
+        id: userId
+      },
+      select: {
+        bio: true,
+        image: true,
+        goal: true,
+        website: true
+      }
+    });
+
+    return NextResponse.json(
+      {
+        profile
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error fetching contributions:", error);
+    return NextResponse.json(
+      { message: "Contributions data could not be retrieved" },
+      { status: 500 }
+    );
+  }
+}
