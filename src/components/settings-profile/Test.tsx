4380983eb4168c6df90634779575cff11074b7e4
yusei53 <yusei530922@gmail.com>
2025-01-03 13:31:50 +0900

    fix: TestUI

--- src/components/settings-profile/Test.tsx
+++ src/components/settings-profile/Test.tsx
@@ -1,24 +1,53 @@
 "use client";
 import Image from "next/image";
+import { Controller, type Control, type FieldErrors } from "react-hook-form";
 import { Box, Typography } from "@mui/material";
+import { ErrorMessage } from "../ui/shared/alert";
+import { Button } from "../ui/shared/button";
 import TextArea from "../ui/shared/text-area/TextArea";
 import { theme } from "@/src/utils/theme";
 
-type TestProps = {
-  image: string;
+type FormValues = {
   username: string;
   bio: string;
   goal: string;
   website: string;
 };
 
-const Test: React.FC<TestProps> = ({ image, username, bio, goal, website }) => {
+type TestProps = {
+  image: string;
+  username: string;
+  control: Control<{
+    username: string;
+    bio: string | null;
+    goal: string | null;
+    website: string | null;
+  }>;
+  isSubmitting: boolean;
+  isSubmitSuccessful: boolean;
+  errors: FieldErrors<FormValues>;
+  onSubmit: (event: React.FormEvent) => Promise<void>;
+};
+
+const Test: React.FC<TestProps> = ({
+  image,
+  username,
+  control,
+  isSubmitting,
+  isSubmitSuccessful,
+  errors,
+  onSubmit
+}) => {
   return (
     <Box
+      my={8}
+      mb={12}
       mx={{ xs: 3, sm: 12 }}
       display={"flex"}
       flexDirection={"column"}
       gap={7}
+      component={"form"}
+      onSubmit={onSubmit}
     >
       <Box
         display={"flex"}
@@ -49,54 +78,89 @@ const Test: React.FC<TestProps> = ({ image, username, bio, goal, website }) => {
           >
             https://www.refty.jp/
           </Typography>
-          <TextArea
-            placeholder="username"
-            rows={1}
-            defaultValue={username}
-            fullWidth
-            multiline={true}
+          <Controller
+            name="username"
+            control={control}
+            render={({ field }) => (
+              <TextArea
+                placeholder="username"
+                rows={1}
+                fullWidth
+                multiline
+                defaultValue={field.value}
+                onChange={field.onChange}
+              />
+            )}
           />
         </Box>
+        {errors.username && <ErrorMessage message={errors.username.message} />}
       </Box>
       <Box>
         <Typography m={0.3}>自己紹介</Typography>
         <Typography m={0.3} fontSize={13} color={`${theme.palette.grey[600]}`}>
           マイページに表示されるプロフィール文です。
         </Typography>
-        <TextArea
-          placeholder="自己紹介(20文字以内)"
-          rows={1}
-          defaultValue={bio}
-          fullWidth
-          multiline={true}
+        <Controller
+          name="bio"
+          control={control}
+          render={({ field }) => (
+            <TextArea
+              placeholder="自己紹介(20文字以内)"
+              rows={1}
+              defaultValue={field.value || ""}
+              fullWidth
+              multiline
+              onChange={field.onChange}
+            />
+          )}
         />
+        {errors.bio && <ErrorMessage message={errors.bio.message} />}
       </Box>
       <Box>
         <Typography m={0.3}>目標設定</Typography>
         <Typography m={0.3} fontSize={13} color={`${theme.palette.grey[600]}`}>
           心に浮かぶ目標や夢を言葉にしてみる場所です。
         </Typography>
-        <TextArea
-          placeholder="〜を達成する、〜みたいになる、〜を目指す(他の人には見えません)"
-          rows={2}
-          defaultValue={goal}
-          fullWidth
-          multiline={true}
+        <Controller
+          name="goal"
+          control={control}
+          render={({ field }) => (
+            <TextArea
+              placeholder="〜を達成する、〜みたいになる、〜を目指す(他の人には見えません)"
+              rows={2}
+              defaultValue={field.value || ""}
+              fullWidth
+              multiline={true}
+              onChange={field.onChange}
+            />
+          )}
         />
+        {errors.goal && <ErrorMessage message={errors.goal.message} />}
       </Box>
       <Box>
         <Typography m={0.3}>ウェブサイト</Typography>
         <Typography m={0.3} fontSize={13} color={`${theme.palette.grey[600]}`}>
           お好きな外部URLをマイページに設置できます。
         </Typography>
-        <TextArea
-          placeholder="https://www.refty.jp/welcome"
-          rows={1}
-          defaultValue={website}
-          fullWidth
-          multiline={false}
+        <Controller
+          name="website"
+          control={control}
+          render={({ field }) => (
+            <TextArea
+              placeholder="https://www.refty.jp/welcome"
+              rows={1}
+              defaultValue={field.value || ""}
+              fullWidth
+              multiline={false}
+              onChange={field.onChange}
+            />
+          )}
         />
+        {errors.website && <ErrorMessage message={errors.website.message} />}
       </Box>
+      <Button type="submit" disabled={isSubmitting || isSubmitSuccessful}>
+        {isSubmitting || isSubmitSuccessful ? "投稿中..." : "投稿する"}
+      </Button>
     </Box>
   );
 };
