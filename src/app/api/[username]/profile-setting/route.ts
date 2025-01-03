7f422297442b29ff8c53460dd9466b4e14558f41
yusei53 <yusei530922@gmail.com>
2025-01-03 13:32:25 +0900

    feat: PATCH API

--- src/app/api/[username]/profile-setting/route.ts
+++ src/app/api/[username]/profile-setting/route.ts
@@ -1,5 +1,7 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import authOptions from "../../auth/[...nextauth]/options";
 import prisma from "@/src/lib/prisma";
 import { getUserIdByUsername } from "@/src/utils/actions/get-userId-by-username";
 
@@ -36,6 +38,38 @@ export async function GET(
     console.error("Error fetching contributions:", error);
     return NextResponse.json(
       { message: "Contributions data could not be retrieved" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PATCH(req: NextRequest) {
+  try {
+    const { username, bio, goal, website } = await req.json();
+
+    const session = await getServerSession(authOptions);
+
+    if (!session?.user.id) {
+      return new NextResponse("認証されていません", { status: 401 });
+    }
+
+    const response = await prisma.user.update({
+      where: {
+        id: session.user.id
+      },
+      data: {
+        username,
+        bio,
+        goal,
+        website
+      }
+    });
+
+    return NextResponse.json(response, { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error creating posts" },
       { status: 500 }
     );
   }
