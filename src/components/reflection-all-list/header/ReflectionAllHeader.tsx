b96038e691b0fb6265ce8bda06ed7f80bc84b2b1
yusei53 <yusei530922@gmail.com>
2025-01-05 15:47:55 +0900

    fix: cssの変更を戻す

--- src/components/reflection-all-list/header/ReflectionAllHeader.tsx
+++ src/components/reflection-all-list/header/ReflectionAllHeader.tsx
@@ -20,7 +20,7 @@ export const ReflectionAllHeader: React.FC<ReflectionAllHeaderProps> = ({
         alignItems={"center"}
         justifyContent={"space-between"}
         mx={{ xs: 4, md: 3 }}
-        mt={8}
+        mt={10}
         mb={2}
       >
         <Typography component={"h1"} fontSize={17} letterSpacing={1}>
