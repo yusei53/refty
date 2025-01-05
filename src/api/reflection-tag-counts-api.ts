20866005a1062c998e242264bb17578ff41c4b90
yusei53 <yusei530922@gmail.com>
2025-01-05 20:32:35 +0900

    feat: reflectionTagCountsAPI

---
+++ src/api/reflection-tag-counts-api.ts
@@ -0,0 +1,18 @@
+import type { ReflectionTagCountList } from "./reflection-api";
+import type { FetchURLOptions } from "../utils/fetchURL";
+import type { Result } from "../utils/types/result";
+import { fetchURL } from "../utils/fetchURL";
+
+type TagCountList = {
+  tagCountList: ReflectionTagCountList;
+};
+
+export const reflectionTagCountsAPI = {
+  async getReflectionTagCountList(): Promise<Result<TagCountList, 500>> {
+    const path = `/api/reflection/tag-counts`;
+    const options: FetchURLOptions = {
+      method: "GET"
+    };
+    return await fetchURL<TagCountList, 500>(path, options);
+  }
+};
