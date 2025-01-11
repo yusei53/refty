740650e3bf6227134c08edac7f51bb73946a2a53
yusei53 <yusei530922@gmail.com>
2025-01-11 11:14:16 +0900

    rename: markdown

--- src/features/routes/post/markdown-editor/StyledEditorContent.tsx
+++ src/features/routes/post/markdown-editor/StyledEditorContent.tsx
@@ -1,6 +1,6 @@
 import { EditorContent } from "@tiptap/react";
 import { styled } from "@mui/material/styles";
-import { sharedMarkdownStyles } from "@/src/features/common/markdown";
+import { sharedMarkdownStyles } from "@/src/features/common/markdown-styles";
 
 export const StyledEditorContent = styled(EditorContent)(({ theme }) => ({
   ...sharedMarkdownStyles(theme)
--- src/features/routes/reflection-detail/article/markdown/StyledMarkdown.tsx
+++ src/features/routes/reflection-detail/article/markdown/StyledMarkdown.tsx
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { sharedMarkdownStyles } from "@/src/features/common/markdown";
+import { sharedMarkdownStyles } from "@/src/features/common/markdown-styles";
 
 export const StyledMarkdown = styled(Box)(({ theme }) => ({
   ...sharedMarkdownStyles(theme)
