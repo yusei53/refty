8cc796f7f8d0824eacbd2794ed472046d97d3c83
yusei53 <yusei530922@gmail.com>
2025-01-12 17:45:41 +0900

    feat: http errorの作成

---
+++ src/utils/http-error.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { HTTPMethod } from "./fetchURL";
+
+export type ErrorCode = 400 | 401 | 403 | 404 | 500;
+
+const httpError = (message: string, status: ErrorCode) =>
+  NextResponse.json({ message }, { status });
+
+export const badRequestError = (message: string) => httpError(message, 400);
+
+export const unauthorizedError = (message: string) => httpError(message, 401);
+
+export const forbiddenError = (message: string) => httpError(message, 403);
+
+export const notFoundError = (message: string) => httpError(message, 404);
+
+export const internalServerError = (
+  httpMethod: HTTPMethod,
+  domain: string,
+  error: unknown,
+  message: string
+) => {
+  console.error(`${domain}の${httpMethod}メソッドで失敗しました`, error);
+  httpError(message, 500);
+};
