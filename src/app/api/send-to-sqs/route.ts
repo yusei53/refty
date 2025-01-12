c661b01617def2b948f7130aead9a3575d62d7f3
yusei53 <yusei530922@gmail.com>
2025-01-12 18:18:06 +0900

    fix: それぞれのapiでも置き換える

--- src/app/api/send-to-sqs/route.ts
+++ src/app/api/send-to-sqs/route.ts
@@ -1,6 +1,7 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { awsSQSService } from "@/src/service/awsSQSService";
+import { internalServerError } from "@/src/utils/http-error";
 
 export async function POST(req: NextRequest) {
   try {
@@ -9,7 +10,6 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(res, { status: 200 });
   } catch (error) {
-    console.error("Error sending to SQS:", error);
-    return new NextResponse("Error sending to SQS:", { status: 500 });
+    return internalServerError("POST", "SQSへのメッセージ送信", error);
   }
 }
