e26c3991a51b8d45a83d1e7ef77dcd9119b946ab
yusei53 <yusei530922@gmail.com>
2025-01-18 14:51:08 +0900

    feat:  book page

---
+++ src/app/(multi-footer)/[username]/book/page.tsx
@@ -0,0 +1,32 @@
+import dynamic from "next/dynamic";
+import { notFound } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { reflectionsBookAPI } from "@/src/api/reflections-book-api";
+import authOptions from "@/src/app/api/auth/[...nextauth]/options";
+import { Loading } from "@/src/components/loading";
+
+const ReflectionsBookPage = dynamic(
+  () => import("./page.client").then((mod) => mod.default),
+  {
+    loading: () => <Loading />
+  }
+);
+
+const page = async ({ params }: { params: { username: string } }) => {
+  const session = await getServerSession(authOptions);
+
+  const res = await reflectionsBookAPI.getReflections(params.username);
+  if (res === 404) {
+    return notFound();
+  }
+
+  return (
+    <ReflectionsBookPage
+      reflections={res.reflections}
+      username={session?.user.username ?? ""}
+      userImage={session?.user.image ?? ""}
+    />
+  );
+};
+
+export default page;
