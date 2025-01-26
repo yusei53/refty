4c7f2531f9e57396593a245259e2250b411bc7e8
yusei53 <yusei530922@gmail.com>
2025-01-26 23:08:00 +0900

    feat: random-post-api

---
+++ src/app/api/[username]/random-reflection/route.ts
@@ -0,0 +1,58 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import prisma from "@/src/lib/prisma";
+import { getUserIdByUsername } from "@/src/utils/actions/get-userId-by-username";
+import {
+  forbiddenError,
+  internalServerError,
+  notFoundError
+} from "@/src/utils/http-error";
+import { getServerSession } from "next-auth";
+import authOptions from "../../auth/[...nextauth]/options";
+
+type Params = {
+  params: {
+    username: string;
+  };
+};
+
+export async function GET(req: NextRequest, { params }: Params) {
+  try {
+    const username = params.username;
+
+    if (!username) {
+      return notFoundError("ユーザーネームが見つかりません");
+    }
+
+    const userId = await getUserIdByUsername(username);
+    const session = await getServerSession(authOptions);
+
+    const isCurrentUser = session?.user.username === username;
+
+    if (!isCurrentUser) {
+      return forbiddenError("権限がありません");
+    }
+
+    if (!userId) {
+      return notFoundError("ユーザーが見つかりません");
+    }
+
+    const randomReflection = await prisma.reflection.findRandom({
+      where: { userId },
+      select: {
+        reflectionCUID: true,
+        title: true,
+        charStamp: true,
+        isPublic: true,
+        isPinned: true,
+        createdAt: true
+      }
+    });
+
+    console.log(randomReflection);
+
+    return NextResponse.json(randomReflection, { status: 200 });
+  } catch (error) {
+    return internalServerError("GET", "ランダムなReflection取得", error);
+  }
+}
