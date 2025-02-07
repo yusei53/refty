270ae6998abbab6841aad6fca815310415d01ee1
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-07 22:54:23 +0900

    fix: 画像の配置

--- src/features/routes/important/second-view/EffectSection.tsx
+++ src/features/routes/important/second-view/EffectSection.tsx
@@ -5,12 +5,16 @@ type EffectSectionProps = {
   image: string;
   title: string;
   description: string;
+  isRightPicture?: boolean;
+  isCenterPicture?: boolean;
 };
 
 const EffectSection: React.FC<EffectSectionProps> = ({
   image,
   title,
-  description
+  description,
+  isRightPicture = false,
+  isCenterPicture = false
 }) => {
   return (
     <Box
@@ -21,10 +25,10 @@ const EffectSection: React.FC<EffectSectionProps> = ({
       mx={15}
     >
       <Typography
-        component={"h3"}
+        component={"h2"}
         fontSize={"1.3rem"}
         fontWeight={"bold"}
-        letterSpacing={{ xs: 1, md: 0.8 }}
+        letterSpacing={{ xs: 1, md: 0.5 }}
         whiteSpace={"pre-line"}
         m={{ xs: "0 auto", md: 0 }}
       >
@@ -38,16 +42,34 @@ const EffectSection: React.FC<EffectSectionProps> = ({
       >
         {description}
       </Typography>
-      <Box m={{ xs: "0 auto", md: 0 }}>
-        <Image
-          src={image}
-          alt={"feature image"}
-          width={220}
-          height={220}
-          priority
-          style={{ margin: "15px 0px" }}
-        />
-      </Box>
+      {isRightPicture && (
+        <Box
+          m={{ xs: "0 auto", md: 0 }}
+          display={"flex"}
+          justifyContent={"right"}
+        >
+          <Image
+            src={image}
+            alt={"feature image"}
+            width={250}
+            height={250}
+            priority
+            style={{ margin: "15px 0px" }}
+          />
+        </Box>
+      )}
+      {isCenterPicture && (
+        <Box m={{ xs: "0 auto", md: "0 auto" }}>
+          <Image
+            src={image}
+            alt={"feature image"}
+            width={250}
+            height={250}
+            priority
+            style={{ margin: "15px 0px" }}
+          />
+        </Box>
+      )}
     </Box>
   );
 };
