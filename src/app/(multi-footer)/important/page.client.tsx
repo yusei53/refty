cd1e3fd488182c761025c84b56c3e2a309d39147
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-07 21:49:42 +0900

    fix

--- src/app/(multi-footer)/important/page.client.tsx
+++ src/app/(multi-footer)/important/page.client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import SecondView from "../../../features/routes/important/second-view/SecondView";
-import FirstView from "@/src/features/routes/welcome/first-view/FirstView";
+import FirstView from "@/src/features/routes/important/first-view/FirstView";
 
 const ImportantPage = () => {
   return (
