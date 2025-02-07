ac5f4c8d37c7a4da30086d6b14bac1fa44f592bb
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-02-07 21:50:12 +0900

    add: metadata

--- src/app/(multi-footer)/important/page.tsx
+++ src/app/(multi-footer)/important/page.tsx
@@ -1,5 +1,8 @@
+import { Metadata } from "next";
 import ImportantPage from "./page.client";
+import { meta } from "@/src/utils/metadata";
 
+export const metadata: Metadata = meta.importantPage;
 const page = () => {
   return (
     <>
