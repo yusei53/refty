a5bdf194b05150e3f9ba5cb116291e6e986344bc
yusei53 <yusei530922@gmail.com>
2025-02-09 19:19:16 +0900

    fixx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,10 +2,9 @@
 import { useMediaQuery } from "@mui/material";
 import { DefaultFooter } from "./default";
 import { MobileFooter } from "./mobile";
-import { theme } from "@/src/utils/theme";
 
 export const Footer = () => {
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const isPWA = useMediaQuery("(display-mode: standalone)");
 
-  return isSmallScreen ? <MobileFooter /> : <DefaultFooter />;
+  return isPWA ? <MobileFooter /> : <DefaultFooter />;
 };
