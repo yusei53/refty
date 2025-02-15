032a4d371872bd0e56a1741b39a2b7a52efc3b6e
na2kera <nat3mitaro@gmail.com>
2025-02-15 20:35:40 +0900

    feat: message

--- src/infrastructure/aws-sqs/aws-sqs.client.ts
+++ src/infrastructure/aws-sqs/aws-sqs.client.ts
@@ -6,6 +6,18 @@ const {
   NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
 } = process.env;
 
+if (!NEXT_PUBLIC_AWS_REGION) {
+  throw new Error("Missing AWS_REGION");
+}
+
+if (!NEXT_PUBLIC_AWS_ACCESS_KEY_ID) {
+  throw new Error("Missing AWS_ACCESS_KEY_ID");
+}
+
+if (!NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY) {
+  throw new Error("Missing AWS_SECRET_ACCESS_KEY");
+}
+
 if (
   !NEXT_PUBLIC_AWS_REGION ||
   !NEXT_PUBLIC_AWS_ACCESS_KEY_ID ||
