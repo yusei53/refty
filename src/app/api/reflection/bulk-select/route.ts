7cebd1cf86109c03a1c13998835acdc94374f24b
yusei53 <yusei530922@gmail.com>
2025-02-16 12:04:50 +0900

    fix: comment and selectedReflectionList

--- src/app/api/reflection/bulk-select/route.ts
+++ src/app/api/reflection/bulk-select/route.ts
@@ -13,7 +13,11 @@ import {
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { reflectionCUID, folderUUID, username } = body;
+    const {
+      reflectionCUID: selectedReflectionList,
+      folderUUID,
+      username
+    } = body;
     const userId = await getUserIdByUsername(username);
 
     const session = await getServerSession(authOptions);
@@ -21,7 +25,7 @@ export async function POST(req: Request) {
       return unauthorizedError("認証されていません");
     }
 
-    if (!Array.isArray(reflectionCUID) || !folderUUID || !userId) {
+    if (!Array.isArray(selectedReflectionList) || !folderUUID || !userId) {
       return badRequestError(
         "投稿一括選択のリクエストパラメータに不整合があります"
       );
@@ -31,13 +35,13 @@ export async function POST(req: Request) {
     const selected = await prisma.reflection.findMany({
       where: {
         reflectionCUID: {
-          in: reflectionCUID
+          in: selectedReflectionList
         },
         userId
       }
     });
 
-    if (selected.length !== reflectionCUID.length) {
+    if (selected.length !== selectedReflectionList.length) {
       return forbiddenError("選択した投稿の中に自分の投稿以外が含まれています");
     }
 
@@ -46,16 +50,19 @@ export async function POST(req: Request) {
         where: {
           userId,
           folderUUID,
-          // MEMO: 現在フォルダに入っているが、今回選択されていない投稿をフォルダから外す
-          reflectionCUID: { notIn: reflectionCUID }
+          // MEMO: 現在あるフォルダに所属している投稿のうち、
+          //       今回選択されなかった投稿（selectedReflectionListに含まれていないもの）
+          //       のフォルダ設定を解除する（folderUUIDをnullにする）。
+          reflectionCUID: { notIn: selectedReflectionList }
         },
         data: { folderUUID: null }
       }),
       prisma.reflection.updateMany({
         where: {
           userId,
-          // MEMO: 選択された投稿をフォルダに入れる
-          reflectionCUID: { in: reflectionCUID }
+          // MEMO: 今回選択された投稿（selectedReflectionListに含まれるもの）
+          //       に対して、指定されたフォルダに設定する（folderUUIDを更新する）。
+          reflectionCUID: { in: selectedReflectionList }
         },
         data: { folderUUID }
       })
