1b183e206d33d2584737d020ed43595bd1f19582
yusei53 <yusei530922@gmail.com>
2025-02-21 09:56:40 +0900

    add: force-dynamic

--- src/app/api/username/check-exists/route.ts
+++ src/app/api/username/check-exists/route.ts
@@ -1,3 +1,10 @@
+export const dynamic = "force-dynamic";
+/*
+おそらく"req.nextUrl.searchParams"はサーバーサイドのみで利用可能あるが、
+RHFのバリデーションチェックで使うため、クライアントサイドで呼ばれている。
+そのためCIで怒られ、回避としてforce-dynamicを付与している
+*/
+
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import prisma from "@/src/lib/prisma";
@@ -5,7 +12,7 @@ import { internalServerError } from "@/src/utils/http-error";
 
 export async function GET(req: NextRequest) {
   try {
-    const username = req.nextUrl.searchParams.get("username");
+    const username = req.nextUrl.searchParams.get("username") ?? undefined;
     if (!username) return;
 
     const user = await prisma.user.findUnique({
