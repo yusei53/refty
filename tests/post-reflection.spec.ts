37a00ef8149fee8aa367e6dcdce6a25fd6639545
na2kera <nat3mitaro@gmail.com>
2025-02-28 20:54:18 +0900

    feat: テストの前にserverを起動

--- tests/post-reflection.spec.ts
+++ tests/post-reflection.spec.ts
@@ -1,21 +1,28 @@
 import { expect, test } from "@playwright/test";
 import { startServer, stopServer } from "./mocks/server";
 
+test.beforeAll(() => {
+  startServer();
+});
+
+test.afterAll(() => {
+  stopServer();
+});
+
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
 test.describe("投稿系", () => {
   test("ログインしていない時投稿できない", async ({ page }) => {
+    stopServer();
     await page.goto("/post");
     await page.goto("/login");
     expect(page.url()).toContain("/login");
   });
 
   test("投稿ページにアクセスできる", async ({ page }) => {
-    startServer();
     await page.goto("/post");
     expect(page.url()).toContain("/post");
-    stopServer();
   });
 });
