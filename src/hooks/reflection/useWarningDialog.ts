24e8131c6cad936dda2ab2d225eb2b1ce111209b
yusei53 <yusei530922@gmail.com>
2025-03-01 20:25:45 +0900

    feat: useWarningDialog

---
+++ src/hooks/reflection/useWarningDialog.ts
@@ -0,0 +1,32 @@
+// hooks/useUnsavedChangesWarning.ts
+import { useEffect, useState } from "react";
+
+export const useWarningDialog = (
+  isDirty: boolean,
+  isSubmitSuccessful: boolean
+) => {
+  const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
+
+  // NOTE: 投稿ページで変更があった場合、または投稿が成功していない場合にhasUnsavedChangesをtrueにする
+  useEffect(() => {
+    if (isDirty && !isSubmitSuccessful) {
+      setHasUnsavedChanges(true);
+    } else {
+      setHasUnsavedChanges(false);
+    }
+  }, [isDirty, isSubmitSuccessful]);
+
+  // NOTE: ページを離れる際に警告ダイアログを表示する
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [hasUnsavedChanges]);
+
+  return hasUnsavedChanges;
+};
