583e51182f91dac8dbfc77a86176544fb32cca27
yusei53 <yusei530922@gmail.com>
2025-03-06 23:32:36 +0900

    feat: e2e/tests/api/post-reflection-api.spec.ts

---
+++ e2e/tests/api/post-reflection-api.spec.ts
@@ -0,0 +1,31 @@
+import { expect, request, test } from "@playwright/test";
+import { authSessionCookie } from "@/e2e/mocks/auth/authSessionCookie";
+import { requestReflection } from "@/e2e/mocks/reflection/reflection";
+
+test.describe("未認証ユーザー", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.context().clearCookies();
+  });
+
+  test("リクエストが拒否され、401エラーが返される", async () => {
+    const apiContext = await request.newContext();
+    const response = await apiContext.post("/api/reflection", {
+      data: requestReflection
+    });
+
+    expect(response.status()).toBe(401);
+  });
+});
+
+test.describe("認証済みユーザー", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.context().addCookies([authSessionCookie]);
+  });
+
+  test("リクエストが許可され、201が返る", async ({ page }) => {
+    const response = await page.request.post("/api/reflection", {
+      data: requestReflection
+    });
+    expect(response.status()).toBe(201);
+  });
+});
