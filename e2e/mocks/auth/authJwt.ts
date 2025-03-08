5018c56b11d82f69b2a9a685e6adc5bafe38c06d
yusei53 <yusei530922@gmail.com>
2025-03-08 09:39:40 +0900

    fix: リフティ公式アカウントのusernameを変更

--- e2e/mocks/auth/authJwt.ts
+++ e2e/mocks/auth/authJwt.ts
@@ -8,7 +8,7 @@ export const authJwt = {
       image:
         "https://lh3.googleusercontent.com/a/ACg8ocJs17E4DFv6dE7fpbJMW8R5ERFBAQIm1-cz4N8XHHk4kOirrJw=s96-c",
       id: "b479ea17-7448-4b6e-9664-0a9588b8ddcc",
-      username: "test"
+      username: "refty"
     };
   }
 };
