6c5bf5093ea748575a59553834fe90c2cb3a2da0
yusei53 <yusei530922@gmail.com>
2025-03-11 00:01:46 +0900

    fix: 音量にフェードインをつける

--- src/hooks/audio/useBGMPlayer.ts
+++ src/hooks/audio/useBGMPlayer.ts
@@ -13,28 +13,56 @@ export const useBGMPlayer = (
   getBGMName: () => string;
 } => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const fadeIntervalRef = useRef<NodeJS.Timeout | null>(null);
   const [currentTrack, setCurrentTrack] = useState<string | null>(null);
 
   const playTrack = async (track: string) => {
-    // NOTE: 既に同じトラックが再生中なら何もしない
-    if (currentTrack === track) {
-      return;
-    }
-    // NOTE: 別のトラックが再生中なら停止
+    // 既に同じトラックが再生中なら何もしない
+    if (currentTrack === track) return;
+
+    // 別のトラックが再生中なら停止
     if (audioRef.current) {
       audioRef.current.pause();
       audioRef.current.currentTime = 0;
+      if (fadeIntervalRef.current) {
+        clearInterval(fadeIntervalRef.current);
+      }
     }
+
     const src = sources[track];
     if (src) {
       audioRef.current = new Audio(src);
       audioRef.current.loop = true;
+
+      const initialVolume = 0.1;
+      const mainVolume = 0.5;
+      const fadeInDuration = 3000;
+      const fadeInInterval = 100;
+      const steps = fadeInDuration / fadeInInterval;
+      const volumeStep = (mainVolume - initialVolume) / steps;
+
+      audioRef.current.volume = initialVolume;
       try {
         await audioRef.current.play();
         setCurrentTrack(track);
       } catch (error) {
         console.error(`音声(${track})の再生に失敗しました:`, error);
+        return;
       }
+
+      // NOTE: フェードイン処理
+      fadeIntervalRef.current = setInterval(() => {
+        if (audioRef.current) {
+          const newVolume = Math.min(
+            audioRef.current.volume + volumeStep,
+            mainVolume
+          );
+          audioRef.current.volume = newVolume;
+          if (newVolume >= mainVolume) {
+            clearInterval(fadeIntervalRef.current!);
+          }
+        }
+      }, fadeInInterval);
     }
   };
 
@@ -44,6 +72,9 @@ export const useBGMPlayer = (
       audioRef.current.currentTime = 0;
       setCurrentTrack(null);
       audioRef.current = null;
+      if (fadeIntervalRef.current) {
+        clearInterval(fadeIntervalRef.current);
+      }
     }
   };
 
