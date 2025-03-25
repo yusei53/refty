12b3bc96acda00e37db5cbbf790a33d049437395
yusei53 <yusei530922@gmail.com>
2025-03-25 23:11:34 +0900

    feat: user-report-api-test

--- e2e/tests/api/user-report-api.spec.ts
+++ e2e/tests/api/user-report-api.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
-import { authJwt } from "@/e2e/mocks/auth/authJwt";
 import { authSessionCookie } from "@/e2e/mocks/auth/authSessionCookie";
+import { refty } from "@/e2e/mocks/auth/user";
 
 test.describe("未認証ユーザー", () => {
   test.beforeEach(async ({ page }) => {
@@ -10,8 +10,30 @@ test.describe("未認証ユーザー", () => {
   test("指定したユーザーネームが存在しない場合、404エラーが返される", async ({
     page
   }) => {
-    const response = await page.request.get("/api/abcdef/report/all-content");
-    expect(response.status()).toBe(404);
+    const responseAllContent = await page.request.get(
+      "/api/abcdef/report/all-content"
+    );
+    expect(responseAllContent.status()).toBe(404);
+
+    const responsePublicPrivate = await page.request.get(
+      "/api/abcdef/report/public-private-reflection"
+    );
+    expect(responsePublicPrivate.status()).toBe(404);
+
+    const responseProfile = await page.request.get(
+      "/api/abcdef/report/profile"
+    );
+    expect(responseProfile.status()).toBe(404);
+
+    const responseReflectionTime = await page.request.get(
+      "/api/abcdef/report/reflection-time"
+    );
+    expect(responseReflectionTime.status()).toBe(404);
+
+    const responseTagCount = await page.request.get(
+      "/api/abcdef/report/tag-count"
+    );
+    expect(responseTagCount.status()).toBe(404);
   });
 });
 
@@ -21,11 +43,29 @@ test.describe("認証済みユーザー", () => {
   });
 
   test("200が返される", async ({ page }) => {
-    // TODO: decodeしなくてもいいmock - userを作成する;
-    const decodedJwt = await authJwt.decode();
-    const response = await page.request.get(
-      `/api/${decodedJwt?.username}/report/all-content`
+    const responseAllContent = await page.request.get(
+      `/api/${refty.username}/report/all-content`
     );
-    expect(response.status()).toBe(200);
+    expect(responseAllContent.status()).toBe(200);
+
+    const responsePublicPrivate = await page.request.get(
+      `/api/${refty.username}/report/public-private-reflection`
+    );
+    expect(responsePublicPrivate.status()).toBe(200);
+
+    const responseProfile = await page.request.get(
+      `/api/${refty.username}/report/profile`
+    );
+    expect(responseProfile.status()).toBe(200);
+
+    const responseReflectionTime = await page.request.get(
+      `/api/${refty.username}/report/reflection-time`
+    );
+    expect(responseReflectionTime.status()).toBe(200);
+
+    const responseTagCount = await page.request.get(
+      `/api/${refty.username}/report/tag-count`
+    );
+    expect(responseTagCount.status()).toBe(200);
   });
 });
