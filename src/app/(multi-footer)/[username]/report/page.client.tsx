917bb1d36daf0e05217e39aafa3a729a4889c913
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-03-25 13:17:03 +0900

    merge

--- src/app/(multi-footer)/[username]/report/page.client.tsx
+++ src/app/(multi-footer)/[username]/report/page.client.tsx
@@ -1,7 +1,11 @@
 "use client";
 import Image from "next/image";
+import { Divider } from "@mui/material";
+import type { ReflectionTagCountList } from "@/src/api/reflection-api";
 import { UserMenuHeaderContainer } from "@/src/features/common/user-menu";
+import { BarChartArea } from "@/src/features/routes/report/BarChartArea";
 import { useIsMobile } from "@/src/hooks/responsive/useIsMobile";
+import { theme } from "@/src/utils/theme";
 
 type UserReportPageProps = {
   currentUsername: string | null;
@@ -12,6 +16,8 @@ type UserReportPageProps = {
   publicCount: number;
   privateCount: number;
   contentLength: number;
+  hourlyPostCount: { hour: number; count: number }[];
+  tagCountList: ReflectionTagCountList;
 };
 
 export const UserReportPage: React.FC<UserReportPageProps> = ({
@@ -22,7 +28,9 @@ export const UserReportPage: React.FC<UserReportPageProps> = ({
   publicCount,
   privateCount,
   contentLength,
-  username
+  username,
+  hourlyPostCount,
+  tagCountList
 }) => {
   const isMobile = useIsMobile();
   return (
@@ -50,6 +58,29 @@ export const UserReportPage: React.FC<UserReportPageProps> = ({
         <Image width={50} height={50} src={image} alt={`${username}の画像`} />
         <p>レポートの公開非公開</p>
         <p>現在{isReportOpen ? "公開" : "非公開"}中</p>
+        <Divider
+          sx={{
+            borderColor: theme.palette.grey[400],
+            marginTop: 6,
+            marginBottom: 4
+          }}
+        />
+        <BarChartArea hourlyPostCount={hourlyPostCount} />
+        <Divider
+          sx={{
+            borderColor: theme.palette.grey[400],
+            marginTop: 6,
+            marginBottom: 4
+          }}
+        />
+        <p>タグカウント</p>
+        <p>
+          {Object.entries(tagCountList).map(([tagName, count]) => (
+            <p key={tagName}>
+              {tagName}: {count}件
+            </p>
+          ))}
+        </p>
       </div>
     </>
   );
