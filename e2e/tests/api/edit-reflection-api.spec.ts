10acb46a97e218fd4b08d98e0a8877764c9e5aab
yusei53 <yusei530922@gmail.com>
2025-03-26 17:44:55 +0900

    feat: edit-api-test

--- e2e/tests/api/edit-reflection-api.spec.ts
+++ e2e/tests/api/edit-reflection-api.spec.ts
@@ -9,7 +9,18 @@ test.describe("未認証ユーザー", () => {
   test.beforeEach(async ({ page }) => {
     await page.context().clearCookies();
   });
-  test("401エラーが返される", async ({ page }) => {
+  test("編集ページにアクセスした場合、401エラーが返される", async ({
+    page
+  }) => {
+    const response = await page.request.get(
+      `/api/reflection/detail/${reflection.reflectionCUID}/edit`
+    );
+    expect(response.status()).toBe(401);
+  });
+
+  test("編集するリクエストが飛ばされた場合、401エラーが返される", async ({
+    page
+  }) => {
     const response = await page.request.patch(
       `/api/reflection/detail/${reflection.reflectionCUID}/edit`,
       {
@@ -25,19 +36,35 @@ test.describe("認証済みユーザー", () => {
     await page.context().addCookies([authSessionCookie]);
   });
 
-  test("指定された投稿のCUIDが存在しない場合、404エラーが返される", async ({
+  test("指定されたreflectionCUIDが自身の投稿でなく、編集ページにアクセスした場合、403エラーが返される", async ({
     page
   }) => {
-    const response = await page.request.patch(
-      `/api/reflection/detail/abcdef/edit`,
-      {
-        data: requestReflection
-      }
+    const response = await page.request.get(
+      // TODO: yusei53の投稿を指定しているので、リフティ公式のサブ垢を作ってCUIDを置き換えたい
+      `/api/reflection/detail/cm8ad31x00003ni588xehdmpg/edit`
+    );
+    expect(response.status()).toBe(403);
+  });
+
+  test("指定されたreflectionCUIDが存在せず、編集ページにアクセスした場合、404エラーが返される", async ({
+    page
+  }) => {
+    const response = await page.request.get(
+      `/api/reflection/detail/not-exist-reflection-cuid/edit`
     );
     expect(response.status()).toBe(404);
   });
 
-  test("指定された投稿のCUIDが自身の投稿でない場合、403エラーが返される", async ({
+  test("指定されたreflectionCUIDが自身の投稿であり、編集ページにアクセスした場合、200が返される", async ({
+    page
+  }) => {
+    const response = await page.request.get(
+      `/api/reflection/detail/${reflection.reflectionCUID}/edit`
+    );
+    expect(response.status()).toBe(200);
+  });
+
+  test("指定されたreflectionCUIDが自身の投稿でなく、編集するリクエストが飛ばされた場合、403エラーが返される", async ({
     page
   }) => {
     const response = await page.request.patch(
@@ -48,7 +75,19 @@ test.describe("認証済みユーザー", () => {
     expect(response.status()).toBe(403);
   });
 
-  test("指定された投稿のCUIDが自身の投稿である場合、200が返される", async ({
+  test("指定されたreflectionCUIDが存在せず、編集するリクエストが飛ばされた場合、404エラーが返される", async ({
+    page
+  }) => {
+    const response = await page.request.patch(
+      `/api/reflection/detail/not-exist-reflection-cuid/edit`,
+      {
+        data: requestReflection
+      }
+    );
+    expect(response.status()).toBe(404);
+  });
+
+  test("指定されたreflectionCUIDが自身の投稿であり、編集するリクエストが飛ばされた場合、200が返される", async ({
     page
   }) => {
     const response = await page.request.patch(
