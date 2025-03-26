9e3654d9fbd321c165c750f2d41b9a1432a87ca6
yusei53 <yusei530922@gmail.com>
2025-03-26 12:01:19 +0900

    fix

--- src/features/routes/report/PostingSummaryStats.tsx
+++ src/features/routes/report/PostingSummaryStats.tsx
@@ -19,52 +19,54 @@ export const PostingSummaryStats: React.FC<PostingSummaryStatsProps> = ({
   const totalPosts = publicCount + privateCount;
 
   return (
-    <Box display={"flex"} alignItems={"center"} gap={2}>
+    <Box display={"flex"} alignItems={"center"} gap={3}>
       <Box>
-        <Typography>投稿数</Typography>
-        <Box display={"flex"} alignItems={"center"} ml={{ xs: 1, sm: 6 }}>
+        <Typography mb={-1}>投稿数</Typography>
+        <Box display={"flex"} alignItems={"center"} ml={{ xs: 1, sm: 5 }}>
           <Typography
             color={theme.palette.grey[600]}
-            fontSize={{ xs: 30, sm: 60 }}
+            fontSize={{ xs: 30, sm: 50 }}
           >
             {totalPosts}
           </Typography>
           <Image
             src={isMobile ? "/curly-brackets-light.png" : "/curly-brackets.png"}
             alt={"curly-brackets"}
-            width={110}
-            height={110}
-            style={{ marginLeft: -25 }}
+            width={120}
+            height={120}
+            style={{ marginLeft: -30 }}
           />
           <Box
             display={"flex"}
             flexDirection={"column"}
-            ml={-4}
+            ml={-5.5}
             height={"100%"}
           >
-            <Box display={"flex"} alignItems={"center"} gap={1} mb={-0.1}>
-              <Typography fontSize={{ xs: 10, sm: 14 }} whiteSpace={"nowrap"}>
-                公開
-              </Typography>
-              <Typography
-                color={theme.palette.grey[600]}
-                fontWeight={550}
-                fontSize={{ xs: 0.875, sm: 18 }}
-              >
-                {publicCount}
-              </Typography>
-            </Box>
-            <Box display={"flex"} alignItems={"center"} gap={1} mt={-0.1}>
-              <Typography fontSize={{ xs: 10, sm: 14 }} whiteSpace={"nowrap"}>
-                非公開
-              </Typography>
-              <Typography
-                color={theme.palette.grey[600]}
-                fontWeight={"bold"}
-                fontSize={{ xs: 0.875, sm: 18 }}
-              >
-                {privateCount}
-              </Typography>
+            <Box display={"flex"} alignItems={"center"} gap={1.5}>
+              <Box display={"flex"} flexDirection={"column"} gap={0.2}>
+                <Typography fontSize={{ xs: 10, sm: 14 }} whiteSpace={"nowrap"}>
+                  公開
+                </Typography>
+                <Typography fontSize={{ xs: 10, sm: 14 }} whiteSpace={"nowrap"}>
+                  非公開
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  color={theme.palette.grey[600]}
+                  fontSize={{ xs: 0.875, sm: 18 }}
+                  mb={-0.4}
+                >
+                  {publicCount}
+                </Typography>
+                <Typography
+                  color={theme.palette.grey[600]}
+                  fontSize={{ xs: 0.875, sm: 18 }}
+                  mt={-0.4}
+                >
+                  {privateCount}
+                </Typography>
+              </Box>
             </Box>
           </Box>
           <CountBarChart
@@ -74,13 +76,18 @@ export const PostingSummaryStats: React.FC<PostingSummaryStatsProps> = ({
         </Box>
       </Box>
       <Box>
-        <Typography>合計文字数</Typography>
-        <Box display={"flex"} ml={{ xs: 1, sm: 8 }} position={"relative"}>
+        <Typography mb={-1}>合計文字数</Typography>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          ml={{ xs: 1, sm: 8 }}
+          position={"relative"}
+        >
           {/* // 画像の高さと合わせるための固定値のheightをセットする */}
-          <Box height={100} />
+          <Box height={120} />
           <Typography
             color={theme.palette.grey[600]}
-            fontSize={{ xs: 30, sm: 60 }}
+            fontSize={{ xs: 30, sm: 50 }}
           >
             {contentLength.toLocaleString()}
           </Typography>
@@ -88,8 +95,8 @@ export const PostingSummaryStats: React.FC<PostingSummaryStatsProps> = ({
             color={theme.palette.grey[600]}
             whiteSpace={"nowrap"}
             position={"absolute"}
-            right={-40}
-            bottom={25}
+            right={-45}
+            bottom={30}
           >
             文字
           </Typography>
