478d5f5ae974f65cf8a6cb87c16837eafa8f3869
kaitobq <kaitobq520@gmail.com>
2025-04-01 17:14:21 +0900

    feature: useResponsive

---
+++ src/hooks/responsive/useResponsive.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useMediaQuery } from "@mui/material";
+import { theme } from "@/src/utils/theme";
+
+interface ResponsiveState {
+  isMobile: boolean;
+  isPWA: boolean;
+}
+
+export const useResponsive = (): ResponsiveState => {
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const [isPWA, setIsPWA] = useState(false);
+
+  useEffect(() => {
+    const checkPWA = () => {
+      // ref: https://developer.mozilla.org/en-US/docs/Web/CSS/@media/display-mode
+      const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
+
+      // ref: https://developer.mozilla.org/en-US/docs/Web/API/Navigator
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const isPWA = isStandalone || (window.navigator as any).standalone;
+      setIsPWA(isPWA);
+    };
+
+    checkPWA();
+    window.matchMedia("(display-mode: standalone)").addEventListener("change", checkPWA);
+
+    return () => {
+      window.matchMedia("(display-mode: standalone)").removeEventListener("change", checkPWA);
+    };
+  }, []);
+
+  return {
+    isMobile,
+    isPWA,
+  };
+};
