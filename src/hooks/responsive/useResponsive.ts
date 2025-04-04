d618eae96e33189bea13a1f1a2624b34fbac6c75
kaitobq <kaitobq520@gmail.com>
2025-04-04 20:39:13 +0900

    fixx

--- src/hooks/responsive/useResponsive.ts
+++ src/hooks/responsive/useResponsive.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useMediaQuery } from "@mui/material";
 import { theme } from "@/src/utils/theme";
 
@@ -9,26 +8,7 @@ type ResponsiveState = {
 
 export const useResponsive = (): ResponsiveState => {
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-  const [isPWA, setIsPWA] = useState(false);
-
-  useEffect(() => {
-    const checkPWA = () => {
-      // ref: https://developer.mozilla.org/en-US/docs/Web/CSS/@media/display-mode
-      const isStandalone = window.matchMedia("(display-mode: standalone)").matches;
-
-      // ref: https://developer.mozilla.org/en-US/docs/Web/API/Navigator
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const isPWA = isStandalone || (window.navigator as any).standalone;
-      setIsPWA(isPWA);
-    };
-
-    checkPWA();
-    window.matchMedia("(display-mode: standalone)").addEventListener("change", checkPWA);
-
-    return () => {
-      window.matchMedia("(display-mode: standalone)").removeEventListener("change", checkPWA);
-    };
-  }, []);
+  const isPWA = useMediaQuery("(display-mode: standalone)");
 
   return {
     isMobile,
