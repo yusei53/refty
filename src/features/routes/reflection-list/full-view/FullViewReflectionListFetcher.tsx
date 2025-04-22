c9c421a6bd9553bf4794d5e3f18c942a65f004ad
na2kera <nat3mitaro@gmail.com>
2025-04-23 01:35:34 +0900

    fix: フォルダ名とコンポーネント名を変更

--- src/features/routes/reflection-list/full-view/FullViewReflectionListFetcher.tsx
+++ src/features/routes/reflection-list/full-view/FullViewReflectionListFetcher.tsx
@@ -2,19 +2,19 @@ import type { ReflectionWithIncludeContent } from "@/src/api/reflection-api";
 import { FullViewMonthlyReflectionListArea } from "./FullViewReflectionPaperListArea";
 import { Loading } from "@/src/components/loading/Loading";
 
-type MonthlyReflectionListFetcherProps = {
+type FullViewReflectionListFetcherProps = {
   reflections: ReflectionWithIncludeContent[];
   username: string;
   userImage: string;
   isReady: boolean;
 };
 
-export const MonthlyReflectionListFetcher = ({
+export const FullViewReflectionListFetcher = ({
   reflections,
   username,
   userImage,
   isReady
-}: MonthlyReflectionListFetcherProps) => {
+}: FullViewReflectionListFetcherProps) => {
   // TODO: Next.jsのバージョンを15に上げ、PPRを実装する際に消す
   if (!isReady) {
     return <Loading />;
