bdd3075e24908394a08ebf0459bd6dd5adc8727b
yusei53 <yusei530922@gmail.com>
2025-05-01 20:00:59 +0900

    update

--- src/app/(multi-footer)/terms/page.tsx
+++ src/app/(multi-footer)/terms/page.tsx
@@ -1,4 +1,8 @@
+import type { Metadata } from "next";
 import TermsPage from "./page.client";
+import { meta } from "@/src/app/_client/utils/metadata";
+
+export const metadata: Metadata = meta.termsPage;
 
 const page = () => {
   return <TermsPage />;
