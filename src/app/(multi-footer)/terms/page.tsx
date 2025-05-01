bc8374581174d830c2fd31ac7f91f23793b548e5
yusei53 <yusei530922@gmail.com>
2025-05-01 19:39:39 +0900

    hotfix: terms page

---
+++ src/app/(multi-footer)/terms/page.tsx
@@ -0,0 +1,7 @@
+import TermsPage from "./page.client";
+
+const page = () => {
+  return <TermsPage />;
+};
+
+export default page;
---
+++ src/app/_client/features/routes/terms/Section.tsx
@@ -0,0 +1,41 @@
+import { Box, Typography } from "@mui/material";
+import { theme } from "../../../utils/theme";
+
+type SectionProps = {
+  title: string;
+  description: string;
+};
+
+const Section: React.FC<SectionProps> = ({ title, description }) => {
+  return (
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      gap={2}
+      margin={"0 auto"}
+      my={8}
+    >
+      <Typography
+        component={"h2"}
+        fontSize={{ xs: "1.2rem", md: "1.3rem" }}
+        fontWeight={550}
+        letterSpacing={{ xs: 1, md: 0.8 }}
+        whiteSpace={"pre-line"}
+        lineHeight={1.8}
+        borderBottom={`1px solid ${theme.palette.grey[400]}`}
+      >
+        {title}
+      </Typography>
+      <Typography
+        letterSpacing={"0.03em"}
+        fontSize={{ xs: 15, md: 17 }}
+        lineHeight={"2rem"}
+        whiteSpace={"pre-line"}
+      >
+        {description}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Section;
