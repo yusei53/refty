e687091be582304a7037830acea55926d4234d18
na2kera <nat3mitaro@gmail.com>
2025-05-04 21:58:34 +0900

    fix: 単数に変更

--- src/app/api/reflection/image/route.ts
+++ src/app/api/reflection/image/route.ts
@@ -2,36 +2,37 @@ import { revalidateTag } from "next/cache";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { internalServerError } from "@/src/app/_server/http-error";
-import { reflectionImagesRepository } from "@/src/app/_server/infrastructure/supabase-storage/reflectionImagesRepository";
+import { reflectionImageRepository } from "@/src/app/_server/infrastructure/supabase-storage/reflectionImageRepository";
 
 export async function POST(req: NextRequest) {
   try {
+    // TODO: 認証の確認する
     // const session = await getUserSession();
     // if (!session) {
     //   return unauthorizedError("認証されていません");
     // }
 
     const formData = await req.formData();
-    const files = formData.getAll("files") as File[];
+    const file = formData.get("file") as File | null;
 
-    if (!files || files.length === 0) {
-      return NextResponse.json({ error: "No files uploaded" }, { status: 400 });
+    if (!file) {
+      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
     }
 
-    const uploadReflectionImageItems = files.map((file) => ({
+    const uploadReflectionImageItem = {
       file,
       path: `reflection-images/${Date.now()}-${file.name}`
-    }));
+    };
 
-    const imageUrls = await reflectionImagesRepository.uploadImages(
-      uploadReflectionImageItems
+    const imageUrl = await reflectionImageRepository.uploadImage(
+      uploadReflectionImageItem
     );
 
-    console.log("imageUrls", imageUrls);
+    console.log("imageUrl", imageUrl);
 
     revalidateTag("reflections-all");
 
-    return NextResponse.json(imageUrls, { status: 201 });
+    return NextResponse.json({ imageUrl }, { status: 201 });
   } catch (error) {
     console.error(error);
     return internalServerError("POST", "新規投稿", error);
