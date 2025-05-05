642e0a1a71202a9ca41095df808b795948671829
yusei53 <yusei530922@gmail.com>
2025-05-05 15:31:07 +0900

    fixx

--- src/app/_client/components/footer/Footer.tsx
+++ src/app/_client/components/footer/Footer.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { DefaultFooter } from "./default";
 import { PWAFooter } from "./mobile";
 import { useResponsive } from "@/src/app/_client/hooks/responsive/useResponsive";
 export const Footer = () => {
   const { isPWA } = useResponsive();
 
-  return isPWA ? <PWAFooter /> : <PWAFooter />;
+  return isPWA ? <PWAFooter /> : <DefaultFooter />;
 };
