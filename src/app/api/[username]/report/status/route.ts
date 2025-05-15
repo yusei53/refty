139bc99f38f8086be48608648ffc4d8c9577a322
kaitobq <kaitobq520@gmail.com>
2025-05-15 16:25:35 +0900

    revert report status

--- src/app/api/[username]/report/status/route.ts
+++ src/app/api/[username]/report/status/route.ts
@@ -1,34 +1,42 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { forbiddenError, notFoundError } from "@/src/app/_server/http-error";
-import { sessionHandler } from "@/src/app/_server/session-handler";
+import {
+  forbiddenError,
+  internalServerError,
+  notFoundError
+} from "@/src/app/_server/http-error";
 import { getUserIdByUsername } from "@/src/app/_shared/actions/get-userId-by-username";
+import { getUserSession } from "@/src/app/_shared/get-user-session";
 import prisma from "@/src/app/_shared/lib/prisma";
 
 export async function GET(
-  req: NextRequest,
+  _: NextRequest,
   { params }: { params: Promise<{ username: string }> }
 ) {
-  return sessionHandler(req, "レポートステータス取得", async ({ session }) => {
-    const { username } = await params;
-    const userId = await getUserIdByUsername(username);
-    if (!userId) {
-      return notFoundError("ユーザーが見つかりません");
-    }
-
+  const { username } = await params;
+  const userId = await getUserIdByUsername(username);
+  if (!userId) {
+    return notFoundError("ユーザーが見つかりません");
+  }
+  const session = await getUserSession();
+  try {
     const user = await prisma.user.findUnique({
       where: { id: userId },
       select: { image: true, isReportOpen: true }
     });
-
     if (user?.isReportOpen === false && session?.username !== username) {
       return forbiddenError("閲覧権限がありません");
     }
-
     return NextResponse.json({
       isReportOpen: user?.isReportOpen,
       userImage: user?.image,
       session
     });
-  });
+  } catch (error) {
+    return internalServerError(
+      "GET",
+      "ユーザーのレポートステータス取得",
+      error
+    );
+  }
 }
